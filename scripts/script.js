cb3fe55158f7d11baca0d3104ec4ade3d5f25375
Nik Vafin <nik_vafin@mail.ru>
2022-03-28 15:23:26 +0300

    Sprint_4_ready

--- scripts/script.js
+++ scripts/script.js
@@ -2,57 +2,40 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
 let popup = document.querySelector('.popup');
-
-
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-
-  let popupNameInput = document.querySelector('.popup__name');
-  let popupAboutInput = document.querySelector('.popup__text');
-  let profileName = document.querySelector('.profile__header');
-  let profileAbout = document.querySelector('.profile__text');
-  profileName.textContent = popupNameInput.value;
-  profileAbout.textContent = popupAboutInput.value;
-  closePopup();
-}
-
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-
+let popupNameInput = document.querySelector('.popup__text-input_type_name');
+let popupAboutInput = document.querySelector('.popup__text-input_type_about');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
 
 function fillPopupForm() {
-  let popupNameInput = document.querySelector('.popup__name');
-  let popupAboutInput = document.querySelector('.popup__text');
-  popupNameInput.value = document.querySelector('.profile__header').innerText;
-  popupAboutInput.value = document.querySelector('.profile__text').innerText;
+  popupNameInput.value = document.querySelector('.profile__name').innerText;
+  popupAboutInput.value = document.querySelector('.profile__about').innerText;
 }
 
 function openPopup() {
-  let popup = document.querySelector('.popup');
   fillPopupForm();
   popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-  let popup = document.querySelector('.popup');
   popup.classList.remove('popup_opened');
 }
 
+function stopScroll(evt) {
+  evt.preventDefault();
+}
 
-
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupNameInput.value;
+  profileAbout.textContent = popupAboutInput.value;
+  closePopup();
+}
 
 formElement.addEventListener("submit", formSubmitHandler);
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
 
-/* Предотвращаем прокрутку страницы при открытом попапе */
-function stopScroll(evt) {
-  evt.preventDefault();
-}
 popup.addEventListener("scroll", stopScroll);
 popup.addEventListener("mousewheel", stopScroll);
 popup.addEventListener("wheel", stopScroll);
