654bd19c5f1da4314f3830fd2f80c6703ee13921
Nik Vafin <nik_vafin@mail.ru>
2022-04-01 17:16:21 +0300

    sprint_4_control_2_fixed

--- scripts/script.js
+++ scripts/script.js
@@ -2,27 +2,45 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
 let popup = document.querySelector('.popup');
-let popupNameInput = document.querySelector('.popup__text-input_type_name');
-let popupAboutInput = document.querySelector('.popup__text-input_type_about');
+let popupNameInput = document.querySelector('.popup__input-name');
+let popupAboutInput = document.querySelector('.popup__input-about');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
+/* Смотри комментарий внизу
+
+function stopScroll(evt) {
+  evt.preventDefault();
+}
+
+function addStopScrollEvent() {
+  popup.addEventListener("scroll", stopScroll);
+  popup.addEventListener("mousewheel", stopScroll);
+  popup.addEventListener("wheel", stopScroll);
+  popup.addEventListener("touchmove", stopScroll);
+}
+
+function deleteStopScrollEvent() {
+  popup.removeEventListener("scroll", stopScroll);
+  popup.removeEventListener("mousewheel", stopScroll);
+  popup.removeEventListener("wheel", stopScroll);
+  popup.removeEventListener("touchmove", stopScroll);
+} */
+
 function fillPopupForm() {
-  popupNameInput.value = document.querySelector('.profile__name').innerText;
-  popupAboutInput.value = document.querySelector('.profile__about').innerText;
+  popupNameInput.value = profileName.textContent;
+  popupAboutInput.value = profileAbout.textContent;
 }
 
 function openPopup() {
   fillPopupForm();
   popup.classList.add('popup_opened');
+/*   addStopScrollEvent(); */
 }
 
 function closePopup() {
+/*   deleteStopScrollEvent(); */
   popup.classList.remove('popup_opened');
-}
-
-function stopScroll(evt) {
-  evt.preventDefault();
 }
 
 function formSubmitHandler (evt) {
@@ -36,7 +54,8 @@ formElement.addEventListener("submit", formSubmitHandler);
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
 
-popup.addEventListener("scroll", stopScroll);
-popup.addEventListener("mousewheel", stopScroll);
-popup.addEventListener("wheel", stopScroll);
-popup.addEventListener("touchmove", stopScroll);
+/* Скролл удален, идея была не в полной блокировки скролла и прокрутки,
+а включении блокировки при открытии поп-апа, потому что насколько я понимаю
+UI - очень неприятно открыть окошко и случайно отмотать страницу вниз.
+Просто реализация подкачала =) Правильно было добавлять обработчики при открытии popup
+и удалять при его закрытии */
